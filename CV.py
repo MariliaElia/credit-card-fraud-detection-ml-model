2bc69d6d25550489ea5c987a3655c06b3bda9241
MariliaElia <melia1@sheffield.ac.uk>
2023-07-27 11:14:39 +0300

    Naive Nayes, GAN architecture test

---
+++ CV.py
@@ -0,0 +1,126 @@
+# Performance metrics libraries
+from sklearn import metrics
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.metrics import roc_auc_score
+
+# Visual Libraries
+from matplotlib import pyplot as plt
+import seaborn as sns
+plt.style.use('ggplot')
+
+#Sampling Libraries
+from imblearn.over_sampling import SMOTE
+from imblearn.under_sampling import RandomUnderSampler
+
+# Imported Libraries
+import numpy as np
+import pandas as pd 
+import copy
+from collections import Counter
+from sklearn.preprocessing import StandardScaler
+from imblearn.pipeline import Pipeline
+
+# Model Libraries
+from sklearn.model_selection import train_test_split, GridSearchCV, StratifiedKFold
+
+
+def visualize_confusion_matrix(cf_matrix, axis):
+    sns.heatmap(pd.DataFrame(cf_matrix), ax=axis, annot=True, cmap="YlGnBu" ,fmt='g')
+    axis.set_title('Confusion matrix', y=1.1)
+    axis.set_ylabel('Actual label')
+    axis.set_xlabel('Predicted label')
+
+def model_scores(y_true, y_pred):
+    print("Accuracy:", accuracy_score(y_true, y_pred))
+    print("Precision:", precision_score(y_true, y_pred))
+    print("Recall:", recall_score(y_true, y_pred))
+    print("F1 score:", f1_score(y_true, y_pred))
+    print("ROC_AUC:", roc_auc_score(y_true, y_pred))
+
+def cross_val_scores(scores):
+    print(f"Accuracy:, {scores['test_accuracy'].mean():0.6f} (+/- {scores['test_accuracy'].std():0.6f})")
+    print(f"Precision: {scores['test_precision'].mean():0.6f} (+/- {scores['test_precision'].std():0.6f})")
+    print(f"Recall: {scores['test_recall'].mean():0.6f} (+/- {scores['test_recall'].std():0.6f})")
+    print(f"F1 score: {scores['test_f1'].mean():0.6f} (+/- {scores['test_f1'].std():0.6f})")
+    print(f"ROC_AUC: {scores['test_roc_auc'].mean():0.6f} (+/- {scores['test_roc_auc'].std():0.6f})")
+
+def plot_roc_curve(model, y_true, X, axis):
+    y_pred_proba = model.predict_proba(X)[::,1]
+
+    fpr, tpr, _ = metrics.roc_curve(y_true,  y_pred_proba)
+    auc = metrics.roc_auc_score(y_true, y_pred_proba)
+
+    #create ROC curve
+    axis.plot(fpr,tpr)
+    axis.set_ylabel('Sensitivity/TPR/Recall')
+    axis.set_xlabel('Specificity/FPR')
+    axis.set_title("ROC-AUC="+str(auc))
+    #plt.legend(loc=4)
+
+def evaluate_model(X_test, y_test, estimator):
+    num_fraud_cases_in_test = len(y_test[y_test==1])
+    num_normal_cases_in_test = len(y_test[y_test==0])
+
+    predictions = estimator.predict(X_test)
+    cm = confusion_matrix(y_test, predictions)
+
+    # Plot normalized confusion matrix and precision recall curve
+    fig, axes = plt.subplots(1,2, figsize=(10,4))
+
+    visualize_confusion_matrix(cm, axes[0])
+
+    plot_roc_curve(estimator, y_test, X_test, axes[1])
+    plt.tight_layout()
+
+    # Print summary    
+    print(f"\nClassified \t{cm[1,1]} out of {num_fraud_cases_in_test} \tfraud cases correctly")
+    print(f"Misclassified \t{cm[0,1]} out of {num_normal_cases_in_test} normal cases")
+
+    model_scores(y_test, predictions)
+
+def model_cv(X_train, y_train, model, score, param_grid, sampling_technique = None):
+    skf = StratifiedKFold(n_splits=5)
+
+    if sampling_technique == 'smote':
+        print("SMOTE applied")
+        pipeline = Pipeline(steps = 
+                            [['pre_process', StandardScaler()],
+                            ['smote', SMOTE(random_state=1)],
+                            ['classifier', model]])
+    elif sampling_technique =='undersampling':
+        pipeline = Pipeline(steps=
+                            [['pre_process', StandardScaler()],
+                            ['undersampling', RandomUnderSampler()],
+                            ['classifier', model]])
+    elif sampling_technique == 'original':
+        print("Original applied")
+        pipeline = Pipeline(steps = 
+                            [['classifier', model]])
+    else:
+        pipeline = Pipeline(steps = 
+                            [['pre_process', StandardScaler()],
+                            ['classifier', model]])
+                
+    grid_search = GridSearchCV(estimator=pipeline,
+                            param_grid=param_grid,
+                            scoring = ['accuracy','precision','recall','f1','roc_auc'],
+                            refit=score,
+                            cv=skf,
+                            n_jobs=-1)
+
+
+    grid_search.fit(X_train, y_train)
+
+    cv_results = pd.DataFrame(grid_search.cv_results_)
+    best_model_results = cv_results.loc[grid_search.best_index_]
+
+    print(f"Accuracy:, {best_model_results['mean_test_accuracy']:0.6f} (+/- {best_model_results['std_test_accuracy']:0.6f})")
+    print(f"Precision: {best_model_results['mean_test_precision']:0.6f} (+/- {best_model_results['std_test_precision']:0.6f})")
+    print(f"Recall: {best_model_results['mean_test_recall']:0.6f} (+/- {best_model_results['std_test_recall']:0.6f})")
+    print(f"F1 score: {best_model_results['mean_test_f1']:0.6f} (+/- {best_model_results['std_test_f1']:0.6f})")
+    print(f"ROC_AUC: {best_model_results['mean_test_roc_auc']:0.6f} (+/- {best_model_results['std_test_roc_auc']:0.6f})")
+
+    print('Best hyperparameters: ', grid_search.best_params_)
+
+    return grid_search
--- GAN.py
+++ GAN.py
@@ -1,105 +1,145 @@
 import numpy as np
 import pandas as pd
+from matplotlib import pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from tensorflow import keras
-from tensorflow.keras import layers
+from tensorflow.keras import layers, regularizers
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.utils import plot_model
+from keras import backend as K
 
 class GAN:
-    def __init__(self, input_dim, generator_output_dim, num_samples, epochs, batch_size):
+    def __init__(self, input_dim, generator_output_dim, discriminator_input_dim, noise_dim, num_samples, epochs, batch_size, dropout):
         self.input_dim = input_dim
         self.generator_output_dim = generator_output_dim
+        self.discriminator_input_dim = discriminator_input_dim
+        self.noise_dim = noise_dim
         self.num_samples = num_samples
         self.epochs = epochs
         self.batch_size = batch_size
+        self.dropout = dropout
 
     def build_discriminator(self):
-        discriminator = keras.Sequential(
-            [
-                layers.Dense(64, input_dim=self.generator_output_dim, activation="relu"),
-                layers.Dense(64, activation="relu"),
-                layers.Dense(1, activation="sigmoid"),
-            ]
-        )
+        discriminator = keras.Sequential([
+            layers.Dense(20, input_dim=self.discriminator_input_dim,
+                         kernel_regularizer=regularizers.l2(0.01)),
+            layers.Dropout(self.dropout),
+            layers.LeakyReLU(alpha=0.2),
+            layers.Dense(10, kernel_regularizer=regularizers.l2(0.01)),
+            layers.Dropout(self.dropout),
+            layers.LeakyReLU(alpha=0.2),
+            layers.Dense(1, activation='sigmoid')
+        ])
      
         plot_model(discriminator, 'discriminator.jpg', show_shapes=True,show_dtype=True)
+        discriminator.compile(
+                    loss="binary_crossentropy",
+                    optimizer=Adam(lr=0.00002, beta_1=0.5),
+                    metrics=["accuracy"],
+                )
         
+        discriminator.summary()
         self.discriminator = discriminator
 
-        return discriminator
-
     def build_generator(self):
-        generator = keras.Sequential(
-            [
-                layers.Dense(64, input_dim=self.input_dim, activation="relu"),
-                layers.Dense(64, activation="relu"),
-                layers.Dense(self.generator_output_dim, activation="sigmoid"),
-            ]
-        )
+        generator = keras.Sequential([
+            layers.Dense(128, input_dim=self.noise_dim, activation='relu', kernel_initializer='glorot_uniform',
+                         kernel_regularizer=regularizers.l2(0.01)),
+            layers.Dropout(self.dropout),
+            layers.Dense(64, activation='relu', kernel_initializer='glorot_uniform',
+                         kernel_regularizer=regularizers.l2(0.01)),
+            layers.Dropout(self.dropout),
+            layers.Dense(self.generator_output_dim, activation='tanh')
+        ])
 
         plot_model(generator, 'generator.jpg', show_shapes=True,show_dtype=True)
+        generator.summary()
         self.generator = generator
     
     def build_gan(self):
         self.discriminator.trainable = False
         gan = keras.Sequential()
-
         gan.add(self.generator)
         gan.add(self.discriminator)
+       
+        gan.compile(loss=self.generator_loss, optimizer=Adam(lr=0.00002, beta_1=0.5))
+        gan.summary()
 
-        self.gan = gan 
-        return gan
+        self.gan = gan
+
+    # create a line plot of loss for the gan and save to file
+    def plot_history(self, d_real_hist, d_fake_hist, g_hist, a_real_hist, a_fake_hist):
+        # plot loss
+        plt.subplot(2, 1, 1)
+        plt.plot(d_real_hist, label='d-real')
+        plt.plot(d_fake_hist, label='d-fake')
+        plt.plot(g_hist, label='gen')
+        plt.legend()
+        # plot discriminator accuracy
+        plt.subplot(2, 1, 2)
+        plt.plot(a_real_hist, label='acc-real')
+        plt.plot(a_fake_hist, label='acc-fake')
+        plt.legend()
+
+        plt.show()
+        # save plot to file
+        plt.savefig('results_gan/plot_line_plot_loss.png')
     
     def train(self, X_train_GAN, y_train_GAN, epochs, batch_size):
         real = np.ones(batch_size)
         fake = np.zeros(batch_size)
+        # prepare lists for storing stats each iteration
+        d_real_hist, d_fake_hist, g_hist, a_real_hist, a_fake_hist = list(), list(), list(), list(), list()
 
         for epoch in range(epochs):
             # Select random samples from the fraudulent training data
             indices = np.random.choice(y_train_GAN, batch_size, replace=False)
             real_samples = X_train_GAN.iloc[indices]
-
-            noise = np.random.normal([batch_size, self.input_dim])
-            latent_points = np.random.normal(0, 1, (batch_size, self.input_dim))
+          
+            latent_points = np.random.normal(0, 1, (batch_size, self.noise_dim))
             generated_samples = self.generator.predict(latent_points)
 
-            # Combine real and fake data
-            X = np.concatenate((real_samples, generated_samples))
-            y = np.concatenate((real, fake))
+            # pass real and generated samples to the discriminator and train on them
+            d_loss_real, d_acc_real = self.discriminator.train_on_batch(real_samples, real)
+            d_loss_fake, d_acc_fake = self.discriminator.train_on_batch(generated_samples, fake)
+            
+            d_loss = 0.5*(d_loss_real + d_loss_fake)
+            d_acc = 0.5*(d_acc_real + d_acc_fake)
 
-            d_loss = self.discriminator.train_on_batch(X, y)
-
-            noise = np.random.normal(0, 1, (batch_size, self.input_dim))
+            noise = np.random.normal(0, 1, (batch_size, self.noise_dim))
             g_loss = self.gan.train_on_batch(noise, real)
 
-            print("%d [D loss: %f, acc.: %.2f%%] [G loss: %f]" %(epoch + 1, d_loss[0], 100 * d_loss[1], g_loss))
+            # record history
+            d_real_hist.append(d_loss_real)
+            d_fake_hist.append(d_loss_fake)
+            g_hist.append(g_loss)
+            a_real_hist.append(d_acc_real)
+            a_fake_hist.append(d_acc_fake)
+
+            print("%d [D loss: %f, acc.: %.2f%%] [G loss: %f]" %(epoch + 1, d_loss, 100 * d_acc, g_loss))
+        
+        self.plot_history(d_real_hist, d_fake_hist, g_hist, a_real_hist, a_fake_hist)
 
     def generate_samples(self, num_samples):
-        noise = np.random.normal(0, 1, (num_samples, self.input_dim))
+        noise = np.random.normal(0, 1, (num_samples, self.noise_dim))
         synthetic_samples = self.generator.predict(noise)
 
         synthetic_X = self.ss.inverse_transform(synthetic_samples)
         synthetic_y = np.ones(num_samples)
         return synthetic_X, synthetic_y
+    
+    # Total Generator Loss
+    def generator_loss(self,y_true, y_pred):
+        bce_loss = keras.losses.binary_crossentropy(y_true, y_pred)
+        kl_loss = keras.losses.kld(y_true, y_pred)
+        total_loss = K.mean(bce_loss + kl_loss)
+        return total_loss
 
     def fit_sample(self, X_train, y_train):
-        discriminator = self.build_discriminator()
-        # compile discriminator architecture
-        discriminator.compile(
-                    loss="binary_crossentropy",
-                    optimizer=Adam(learning_rate=0.0001, beta_1=0.5),
-                    metrics=["accuracy"],
-                )
-
+        self.build_discriminator()
         self.build_generator()
-
-        # make weights in the discriminator not trainable
-        discriminator.trainable = False
-
-        cgan = self.build_gan()
-        cgan.compile(loss='kld', optimizer=Adam(lr=0.0001, beta_1=0.5))
-
+        self.build_gan()
+        
         self.ss = StandardScaler()
         Xtrain_1 = X_train[y_train == 1]
         X_train_GAN = pd.DataFrame(self.ss.fit_transform(Xtrain_1[:]), index=Xtrain_1.index)
@@ -113,6 +153,7 @@ class GAN:
         y_new = np.concatenate((y_train, synthetic_y))
 
         return(X_new, y_new)
+
 
 
 
